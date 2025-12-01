c1d6497e46ceb82c41a118326d1ae31cd9327989
2ndrushedd-spec <2ndrushedd@gmail.com>
2025-12-01 13:11:55 +0800

    GHPages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InstrumentSelect from "./pages/InstrumentSelect";
 import PianoModule from "./pages/PianoModule";
 import Lesson1 from "./pages/lessons/Lesson1";
@@ -6,13 +6,13 @@ import Lesson2 from "./pages/lessons/Lesson2"; // <-- added import
 
 export default function App() {
 	return (
-		<Router>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/" element={<InstrumentSelect />} />
 				<Route path="/piano" element={<PianoModule />} />
 				<Route path="/lesson/1" element={<Lesson1 />} />
 				<Route path="/lesson/2" element={<Lesson2 />} /> {/* <-- new route */}
 			</Routes>
-		</Router>
+		</BrowserRouter>
 	);
 }
